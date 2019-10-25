f94f779f0db3f289ea879088e959a091a00b2d20
mi <clock2elizabeth@gmail.com>
2019-10-25 19:02:07 +0900

    Update CRUD system

--- index.js
+++ index.js
@@ -23,42 +23,47 @@ app.post("/api/create", (req, res, next) => {
   var card_name = req.body.postCardName;
   var card_info = req.body.postCardInfo;
   var user_id = req.body.postUserId;
-  var qr = Math.round(Math.random() * 10000);
+  var url_num = Math.round(Math.random() * 10000);
   console.log(card_name);
   console.log(card_info);
   console.log(user_id);
-  console.log(qr);
+  console.log(url_num);
   db.pool.connect( async (err, client) => {
     if (err) {
       console.log(err);
     } else {
-      // qrコード用のURL作成
       var slct = "SELECT MAX(card_id) FROM cards";
       client.query(slct, (err, result) => {
+        var prvs_card_id;
         if (err) {
           console.log(err);
         } else {
           console.log(result.rows);
-          rslt = result.rows[0].max;
-          console.log(rslt);
-          qr = "" + rslt + qr + rslt;
-          Number(qr);
-          console.log(qr);
+          prvs_card_id = result.rows[0].max;
+          console.log(prvs_card_id);
+          url_num = "" + prvs_card_id + url_num + prvs_card_id;
+          Number(url_num);
+          console.log(url_num);
         }
         
-        // DBに新カード登録
-        var insrt = "INSERT INTO cards (card_name, card_info, qr, admin) VALUES ($1, $2, $3, $4)"
-        client.query(insrt, [card_name, card_info, qr, user_id], (err, result) => {
+        var insrt = "INSERT INTO cards (card_name, card_info, url_num) VALUES ($1, $2, $3)";
+        client.query(insrt, [card_name, card_info, url_num], (err, result) => {
           if (err) {
             console.log(err);
           }
         });
 
-        // レスポンス
+        insrt = "INSERT INTO admin (fk_card_id, fk_user_id) VALUES ($1, $2)";
+        client.query(insrt, [prvs_card_id, user_id], (err) => {
+          if (err) {
+            console.log(err);
+          }
+        });
+
         res.json({
           cardName: card_name,
           cardInfo: card_info,
-          qr : qr
+          cardUrl : url_num
         }); 
       });
     }
@@ -102,25 +107,25 @@ app.post("/api/edit", (req, res, next) => {
 // ポイント付与
 app.post("/api/add", (req, res, next) => {
   console.log("api/add");
-  var qr = req.body.postQr;
+  var url_num = req.body.postUrlNum;
   var user_id = req.body.postUesrId;
-  console.log(qr);
+  console.log(url_num);
   console.log(user_id);
-  var point_after;
   db.pool.connect( async (err, client) => {
+    var point_after = 0;
     if (err) {
       console.log(err);
     } else {
       var card_id;
-      var slct = "SELECT card_id FROM cards WHERE qr = $1";
-      await client.query(slct, [qr], (err, result) =>  {
+      
+      var slct = "SELECT card_id FROM cards WHERE url_num = $1";
+      client.query(slct, [url_num], (err, result) =>  {
         if (err) {
           console.log(err);
         } else {
           card_id = result.rows[0].card_id;
           console.log(card_id);
         }
-        // return card_id;
 
         slct = "SELECT point_sum FROM points WHERE fk_card_id = $1 AND fk_user_id = $2";
         client.query(slct, [card_id, user_id], (err, result) => {
@@ -133,33 +138,19 @@ app.post("/api/add", (req, res, next) => {
             point_after = rslt + 1;
             console.log(point_after);
           }
-          return point_after;
+
+          var updt = "UPDATE points SET point_sum = $1 WHERE fk_card_id = $2 AND fk_user_id = $3";
+          client.query(updt, [point_after, card_id, user_id], (err, result) => {
+            if (err) {
+              console.log(err);
+            } else {
+              res.json({
+                point: point_after
+              });
+            }
+          });
         });
-        return point_after;
-      });
-      // slct = "SELECT point_sum FROM points WHERE fk_card_id = $1 AND fk_user_id = $2";
-      // await client.query(slct, [card_id, user_id], (err, result) => {
-      //   if (err) {
-      //     console.log(err);
-      //   } else {
-      //     console.log(result.rows);
-      //     var rslt = result.rows[0].point_sum;
-      //     console.log(rslt);
-      //     point_after = rslt + 1;
-      //     console.log(point_after);
-      //   }
-      //   return point_after;
-      // });
-      var updt = "UPDATE points SET point_sum = $1";
-      await client.query(updt, [point_after], (err, result) => {
-        if (err) {
-          console.log(err);
-        }
       });
     }
-    return point_after;
-  });
-  res.json({
-    point: point_after
   });
 });
