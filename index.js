1572c3cad074e4c8a14713ccd2ea2a4f79ae8cc6
mi <clock2elizabeth@gmail.com>
2019-10-27 14:33:56 +0900

    Update port

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const app = express();
 const db = require("./db/dbinfo");
-require('dotenv').config();
-const port = process.env.ENV_PORT || 5000;
+const port = process.env.PORT || 5000;
 
 app.use(express.json()); //いらないかも
 app.use(express.urlencoded({ extended: true }));
